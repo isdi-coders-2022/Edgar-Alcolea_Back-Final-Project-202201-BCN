55f5716fc41d8f8aba6874e05e60f6aea6b4049d
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-08 12:39:03 +0100

    Added jest to eslintrc.js env config

---
+++ src/middlewares/errors.test.js
@@ -0,0 +1,5 @@
+describe("Given a notFoundError middleware", () => {
+  describe("When it receives a response", () => {
+    test("Then it should call the response status with 404", () => {});
+  });
+});
