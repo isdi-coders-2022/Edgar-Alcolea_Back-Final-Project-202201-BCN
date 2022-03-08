e1d22a88571d46e8a71786e7b38637c68b6d9b67
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-08 19:06:56 +0100

    Added Spot model

---
+++ src/db/models/Spot.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const spotSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  marked: {
+    type: Number,
+    default: 0,
+  },
+  description: {
+    type: String,
+    minlength: 20,
+    maxlength: 300,
+  },
+  createdBy: { type: Schema.Types.ObjectId },
+  location: {
+    type: String,
+    minlength: 10,
+    maxlength: 100,
+  },
+  coordinates: {
+    type: [Schema.Types.Decimal128],
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Spot = model("Spot", spotSchema, "spots");
+
+export default Spot;
