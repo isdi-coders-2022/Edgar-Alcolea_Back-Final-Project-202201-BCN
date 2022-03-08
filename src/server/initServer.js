4cd28c73bd57430e21ce63255cd1ee935f0228e1
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-08 11:55:31 +0100

    Created initServer.js

--- src/server/initServer.js
+++ src/server/initServer.js
@@ -1,5 +1,14 @@
+const chalk = require("chalk");
 const debug = require("debug");
 
-const initServer = (app, port) => {
-  const server = app.listen(port, () => {});
-};
+const initServer = (app, port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.greenBright(`Server listening on http://localhost:${port}`));
+      resolve();
+    });
+
+    server.on("error", (error) => reject(error));
+  });
+
+module.export = initServer;
