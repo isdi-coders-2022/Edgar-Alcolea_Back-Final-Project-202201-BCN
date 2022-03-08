acbb35534847b2c253e0f46aafd5fd7c12f0b8f3
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-08 13:55:45 +0100

    Added connectDB to index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ require("dotenv").config();
 const debug = require("debug")("PK-spots:root");
 const chalk = require("chalk");
 const express = require("express");
+const connectDB = require("./db/connectDB");
 const initServer = require("./server/initServer");
 
 const app = express();
 const port = process.env.PORT || 4000;
+const mongoUri = process.env.MONGO_CONNECT;
 
 (async () => {
   try {
     await initServer(app, port);
+    await connectDB(mongoUri);
     debug(chalk.bgGray.greenBright(`Connection succesful!`));
   } catch (error) {
     debug(chalk.redBright(`Error: ${error.message}`));
