6a2015a36080b82298f3dbae1afe4068d0d3b46d
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-09 10:46:24 +0100

    Tested /spots get endpoint

--- src/server/routers/spotsRouter.test.js
+++ src/server/routers/spotsRouter.test.js
@@ -1,8 +1,9 @@
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const { default: mongoose } = require("mongoose");
+const request = require("supertest");
 const connectDB = require("../../db/connectDB");
 const Spot = require("../../db/models/Spot");
-
+const app = require("../index");
 require("dotenv").config();
 
 let mongoDB;
@@ -20,7 +21,7 @@ beforeEach(async () => {
       marked: 100,
       description:
         "Indoor facility with crazy props and themed spots, training programs and gym.",
-      createdBy: "testID",
+      createdBy: "62274dd6fb4746a872d98b8d",
       location: "Next to L.A.X airport",
       coordinates: [33.920548123347544, -118.33193817357487],
       image: "testImg",
@@ -29,8 +30,8 @@ beforeEach(async () => {
       name: "Test Place",
       marked: 20,
       description: "A place that exists just for the purpose of testing.",
-      createdBy: "testID",
-      location: "The mind",
+      createdBy: "62274dd6fb4746a872d98b8d",
+      location: "The mind inside its creator",
       coordinates: [0, 0],
       image: "testImg",
     }
@@ -48,6 +49,14 @@ afterAll(async () => {
 
 describe("Given a /spots endpoint", () => {
   describe("When it receives a GET request", () => {
-    test("Then it should respond with 200 status code and an array of Spots");
+    test("Then it should respond with 200 status code and an array of Spots", async () => {
+      const expectedLength = 2;
+      const expectedName = "Tempest Freerunning Academy";
+
+      const { body } = await request(app).get("/spots").expect(200);
+
+      expect(body).toHaveLength(expectedLength);
+      expect(body[0]).toHaveProperty("name", expectedName);
+    });
   });
 });
