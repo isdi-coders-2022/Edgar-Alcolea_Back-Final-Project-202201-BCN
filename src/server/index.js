fef1b9a8b73c9b390f0b9e86dfa97c3660e7b6b3
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-09 09:31:41 +0100

    Added helmet and errors middlewares to main chain

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const helmet = require("helmet");
+const { generalError, notFoundError } = require("../middlewares/errors");
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.static("uploads"));
+app.use(helmet());
+
+app.use("/spots", spotRouter);
+
+app.use(notFoundError);
+app.use(generalError);
+
 module.exports = app;
