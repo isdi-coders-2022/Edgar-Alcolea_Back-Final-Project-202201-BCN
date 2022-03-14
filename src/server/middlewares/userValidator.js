98aafaffc2358545a6bd5b9b374f7524d8d9d96a
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-14 23:27:32 +0100

    Tested userValidator

--- src/server/middlewares/userValidator.js
+++ src/server/middlewares/userValidator.js
@@ -1,15 +1,13 @@
 const { Joi } = require("express-validation");
 
-const userValidator = {
-  body: Joi.object({
-    name: Joi.string().required(),
-    userName: Joi.string().required(),
-    password: Joi.string().required(),
-    age: Joi.number(),
-    bio: Joi.string().min(20).max(300),
-    city: Joi.string(),
-    image: Joi.string().required(),
-  }),
-};
+const userValidator = Joi.object({
+  name: Joi.string().required(),
+  userName: Joi.string().required(),
+  password: Joi.string().required(),
+  age: Joi.number(),
+  bio: Joi.string().min(20).max(300),
+  city: Joi.string(),
+  image: Joi.string().required(),
+});
 
 module.exports = userValidator;
