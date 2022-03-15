110247cb5cc17d4882fcdad4955627dd0ad6e3a7
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-15 13:45:49 +0100

    Installed multer

--- src/server/routers/spotsRouter.js
+++ src/server/routers/spotsRouter.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { getSpots, deleteSpot } = require("../controllers/spotsControllers");
+const { validate } = require("express-validation");
+const {
+  getSpots,
+  deleteSpot,
+  createSpot,
+} = require("../controllers/spotsControllers");
+const spotValidator = require("../middlewares/spotValidator");
 
 const router = express.Router();
 
 router.get("/", getSpots);
 router.delete("/delete/:spotId", deleteSpot);
+router.post("/new", validate(spotValidator), createSpot);
 
 module.exports = router;
