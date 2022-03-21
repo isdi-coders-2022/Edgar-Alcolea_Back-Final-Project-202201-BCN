f8a13eb3aa35fae31c6e56e408dec37c7517f8e9
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-21 10:19:27 +0100

    Added and tested auth middleware

---
+++ src/server/middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const headerAuth = req.header("Authorization");
+  if (!headerAuth) {
+    const error = new Error("Token missing");
+    error.code = 401;
+    next(error);
+  } else {
+    const token = headerAuth.replace("Bearer ", "");
+    try {
+      jwt.verify(token, process.env.JWT_SECRET);
+      next();
+    } catch (error) {
+      error.code = 401;
+      next(error);
+    }
+  }
+};
+
+module.exports = auth;
