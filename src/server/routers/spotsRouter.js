637c7da960b201af5ae61226ab59e2ad03a954a7
Edgar Alcolea <edgaralcolea@gmail.com>
2022-03-21 10:40:15 +0100

    Refactorized tests to include authorization

--- src/server/routers/spotsRouter.js
+++ src/server/routers/spotsRouter.js
@@ -8,6 +8,7 @@ const {
   updateSpot,
   getSpot,
 } = require("../controllers/spotsControllers");
+const auth = require("../middlewares/auth");
 const spotValidator = require("../middlewares/spotValidator");
 
 const upload = multer({
@@ -21,15 +22,17 @@ const spotsRouter = express.Router();
 
 spotsRouter.get("/", getSpots);
 spotsRouter.get("/:id", getSpot);
-spotsRouter.delete("/delete/:spotId", deleteSpot);
+spotsRouter.delete("/delete/:spotId", auth, deleteSpot);
 spotsRouter.post(
   "/new",
+  auth,
   validate(spotValidator),
   upload.single("image"),
   createSpot
 );
 spotsRouter.put(
   "/:id",
+  auth,
   validate(spotValidator),
   upload.single("image"),
   updateSpot
